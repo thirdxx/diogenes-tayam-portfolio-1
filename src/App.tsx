7d3ce5d225bc6a54739fe8d6ec8b0e98699432c4
Diogenes Tayam <125981474+thirdxx@users.noreply.github.com>
2025-08-20 14:55:02 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <Sonner />
         <HashRouter>
           <Routes>
-            <Route path="/diogenes-tayam-portfolio-1/" element={<Index />} />
+            <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
